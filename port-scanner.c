ad8a835f3f18c6ef263065feace96a8956fd4d1d
michaelnkr808 <226908033+michaelnkr808@users.noreply.github.com>
2026-08-13 15:59:32 -0700

    looping logic on naive sequential scanner

--- port-scanner.c
+++ port-scanner.c
@@ -5,10 +5,12 @@
 #include <string.h>
 #include <sys/socket.h>
 #include <sys/types.h>
+#include <unistd.h>
 
 int main(void) {
   int sfd; // socket file descriptor
-  const char *localhost = "127.0.0.1";
+  const char *localhost = "scanme.nmap.org";
+  char str[6];
   struct addrinfo hints;
   struct addrinfo *servinfo;
 
@@ -16,23 +18,34 @@ int main(void) {
   hints.ai_family = AF_INET;        // ipv4 or ipv6, dont care
   hints.ai_socktype = SOCK_STREAM;  // tcp
 
-  if ((sfd = getaddrinfo(localhost, "7000", &hints, &servinfo)) != 0) {
-    fprintf(stderr, "gai error: %s\n", gai_strerror(sfd));
-    exit(1);
+  for (int i = 8080; i < 8081; i++) {
+    snprintf(str, sizeof(str), "%d", i);
+    if ((sfd = getaddrinfo(localhost, str, &hints, &servinfo)) != 0) {
+      fprintf(stderr, "Port Number: %d gai error: %s\n", i, gai_strerror(sfd));
+      exit(1);
+    }
+    sfd = socket(servinfo->ai_family, servinfo->ai_socktype,
+                 servinfo->ai_protocol);
+
+    int c = connect(sfd, servinfo->ai_addr, servinfo->ai_addrlen);
+
+    if (c == -1) {
+      if (errno == ECONNREFUSED) {
+        printf("Port Number: %d Socket Closed\n", i);
+      } else if (errno == EHOSTUNREACH) {
+        printf("Port Number: %d Host is unreachable\n", i);
+      } else if (errno == ENETUNREACH) {
+        printf("Port Number: %d Network is unreachable\n", i);
+      } else {
+        printf("Port Number: %d Unknown Error\n", i);
+        printf("%s\n", strerror(errno));
+      }
+
+    } else {
+      printf("Port Number: %d Connected!\n", i);
+    }
+
+    close(sfd);
+    freeaddrinfo(servinfo);
   }
-
-  sfd =
-      socket(servinfo->ai_family, servinfo->ai_socktype, servinfo->ai_protocol);
-
-  int c = connect(sfd, servinfo->ai_addr, servinfo->ai_addrlen);
-
-  if (c == -1) {
-    fprintf(stderr, "Connection Errored\n");
-    printf("%s\n", strerror(errno));
-  } else {
-    printf("Okay nice you connected\n");
-    // here i implement sequential checking of ports
-  }
-
-  freeaddrinfo(servinfo);
 }
